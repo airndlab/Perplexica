c70490a5d06fac82161696ff5efd6e65fdafec9e
Grig Alex <alexengrigdev@gmail.com>
2024-11-10 16:52:51 +0300

    Fix link

--- ui/app/discover/page.tsx
+++ ui/app/discover/page.tsx
@@ -86,7 +86,7 @@ const Page = () => {
           {discover &&
             discover?.map((item, i) => (
               <Link
-                href={`/?q=Сводка: ${item.url}`}
+                href={`/?q=Сводка: ${item.url.includes('/') ? item.url.split('/').pop() : item.url}`}
                 key={i}
                 className="max-w-sm rounded-lg overflow-hidden bg-light-secondary dark:bg-dark-secondary hover:-translate-y-[1px] transition duration-200 relative"
               >
